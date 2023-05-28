b6bf108e8bdc053d6ea2f21ba239e735ce198125
Bruno César <bcgmeireles@gmail.com>
2023-05-28 08:38:51 -0300

    implementing bottom nav bar

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
 
-class BaseScreen extends StatelessWidget {
-  BaseScreen({super.key});
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({super.key});
 
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
+  int currentIndex = 0;
+
+  List pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      children: [
-        Container(
-          color: Colors.red,
-          child: TextButton(
-              onPressed: () {
-                pageController.jumpToPage(1);
-              },
-              child: const Text(
-                'Próximo',
-                style: TextStyle(color: Colors.white),
-              )),
-        ),
-        Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-      ],
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        elevation: 2,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        ],
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
