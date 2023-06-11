bf77e3906bf9beda80bff43c07ae23448f8ebbd5
Bruno César <bcgmeireles@gmail.com>
2023-06-11 13:08:16 -0300

    login screen design update

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -12,84 +12,112 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: kSecondaryColor,
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Login'),
+        backgroundColor: kSecondaryColor,
       ),
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.symmetric(horizontal: 15),
-          color: kPrimaryColor.withOpacity(.9),
-          child: Form(
-            key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              shrinkWrap: true,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: TextStyle(
-                      color: Color(0x95FFFFFF),
+      body: ListView(
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 50, bottom: 35),
+            height: 150,
+            child: Image.asset('assets/images/logo_vallo_transparent.png'),
+          ),
+          Center(
+            child: Form(
+              key: formKey,
+              child: ListView(
+                physics: const NeverScrollableScrollPhysics(),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                shrinkWrap: true,
+                children: [
+                  Card(
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    color: const Color(0xFFD6D6D6),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: Color(0xFF646464),
+                        ),
+                      ),
+                      keyboardType: TextInputType.text,
+                      autocorrect: false,
+                      style: const TextStyle(color: Color(0xFF646464)),
+                      validator: (email) {
+                        if (!emailValid(email!)) {
+                          return 'Email Inválido!';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  keyboardType: TextInputType.text,
-                  autocorrect: false,
-                  style: const TextStyle(color: Colors.white),
-                  validator: (email) {
-                    if (!emailValid(email!)) {
-                      return 'Email Inválido!';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Senha',
-                    hintStyle: TextStyle(
-                      color: Color(0x95FFFFFF),
+                  const SizedBox(height: 10),
+                  Card(
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    color: const Color(0xFFD6D6D6),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        hintText: 'Senha',
+                        hintStyle: TextStyle(
+                          color: Color(0xFF646464),
+                        ),
+                      ),
+                      autocorrect: false,
+                      style: const TextStyle(color: Color(0xFF646464)),
+                      obscureText: true,
+                      validator: (password) {
+                        if (password == null || password.length < 6) {
+                          return 'Senha Inválida';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  autocorrect: false,
-                  style: const TextStyle(color: Colors.white),
-                  obscureText: true,
-                  validator: (password) {
-                    if (password == null || password.length < 6) {
-                      return 'Senha Inválida';
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Esqueci minha senha!',
-                      style: TextStyle(color: Colors.white),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.only(right: 6),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Esqueci minha senha',
+                        style: TextStyle(color: kPrimaryColor),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8))),
-                  onPressed: () {
-                    formKey.currentState!.validate();
-                  },
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(color: kPrimaryColor, fontSize: 16),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: kPrimaryColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                      onPressed: () {
+                        formKey.currentState!.validate();
+                      },
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(color: kSecondaryColor, fontSize: 16),
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
